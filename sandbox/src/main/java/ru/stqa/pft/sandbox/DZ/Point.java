0dcc25e9b80415db059266861cc5ed80fadbe97c
YNWAA <pele_2092@mail.ru>
2021-07-10 20:59:34 +0300

    Исправлено замечание с Задания №1

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DZ/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DZ/Point.java
@@ -7,10 +7,10 @@ public class Point {
     public static void main(String[] args) {
         Point p1 = new Point();
         Point p2 = new Point();
-        p1.x1 = -4;
-        p1.y1 = 10;
-        p2.x2 = 0;
-        p2.y2 = 13;
+        p1.x1 = 1;
+        p1.y1 = 5;
+        p2.x2 = 8;
+        p2.y2 = 4;
         System.out.println( "Расстояние между точками P1(" + p1.x1 + ";" + p1.y1 + ") и (" + p2.x2 + ";" + p2.y2 + ") = " + distance( p1, p2 ) );
     }
 
