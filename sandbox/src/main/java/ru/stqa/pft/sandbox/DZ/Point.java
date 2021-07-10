2730424fb5ab6f645afd6a9f65276691e5655c75
YNWAA <pele_2092@mail.ru>
2021-07-10 21:00:33 +0300

    Update Point.java

--- sandbox/src/main/java/ru/stqa/pft/sandbox/DZ/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/DZ/Point.java
@@ -11,7 +11,7 @@ public class Point {
         p1.y1 = 5;
         p2.x2 = 8;
         p2.y2 = 4;
-        System.out.println( "Расстояние между точками P1(" + p1.x1 + ";" + p1.y1 + ") и (" + p2.x2 + ";" + p2.y2 + ") = " + distance( p1, p2 ) );
+        System.out.println( "Расстояние между точками P1(" + p1.x1 + ";" + p1.y1 + ") и P2(" + p2.x2 + ";" + p2.y2 + ") = " + distance( p1, p2 ) );
     }
 
     public static double distance(Point p1, Point p2) {
