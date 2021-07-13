b1b0cd2de831bf909d6c0c332fbbc720c3f940a1
YNWAA <pele_2092@mail.ru>
2021-07-13 19:51:06 +0300

    Выделил из кода теста вспомогательные методы, в том числе метод заполнения формы.

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/ContactCreationTests.java
@@ -1,61 +1,92 @@
 package ru.stqa.pft.addressbook;
 
-import java.util.regex.Pattern;
 import java.util.concurrent.TimeUnit;
 import org.testng.annotations.*;
-import static org.testng.Assert.*;
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.Select;
 
 public class ContactCreationTests {
-  private WebDriver driver;
+  private WebDriver dr;
 
-  @BeforeClass(alwaysRun = true)
+  @BeforeMethod(alwaysRun = true)
   public void setUp() throws Exception {
-    driver = new FirefoxDriver();
-    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    dr = new FirefoxDriver();
+    dr.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+    dr.get( "http://localhost/addressbook/index.php" );
   }
 
   @Test
   public void ContactCreationTests() throws Exception {
-    driver.get("http://localhost/addressbook/index.php");
-    driver.findElement(By.name("user")).click();
-    driver.findElement(By.name("user")).clear();
-    driver.findElement(By.name("user")).sendKeys("admin");
-    driver.findElement(By.name("pass")).click();
-    driver.findElement(By.name("pass")).clear();
-    driver.findElement(By.name("pass")).sendKeys("secret");
-    driver.findElement(By.xpath("//input[@value='Login']")).click();
-    driver.findElement(By.linkText("add new")).click();
-    driver.findElement(By.name("firstname")).click();
-    driver.findElement(By.name("firstname")).clear();
-    driver.findElement(By.name("firstname")).sendKeys("test1");
-    driver.findElement(By.name("middlename")).clear();
-    driver.findElement(By.name("middlename")).sendKeys("test2");
-    driver.findElement(By.name("lastname")).clear();
-    driver.findElement(By.name("lastname")).sendKeys("test3");
-    driver.findElement(By.name("nickname")).clear();
-    driver.findElement(By.name("nickname")).sendKeys("test4");
-    driver.findElement(By.name("address")).clear();
-    driver.findElement(By.name("address")).sendKeys("test5");
-    driver.findElement(By.name("home")).clear();
-    driver.findElement(By.name("home")).sendKeys("test6");
-    driver.findElement(By.name("email")).clear();
-    driver.findElement(By.name("email")).sendKeys("test7");
-    driver.findElement(By.xpath("//div[@id='content']/form/input[21]")).click();
-    driver.findElement(By.linkText("home")).click();
+    dr.findElement(By.name("user")).click();
+    dr.findElement(By.name("user")).clear();
+    dr.findElement(By.name("user")).sendKeys("admin");
+    dr.findElement(By.name("pass")).click();
+    dr.findElement(By.name("pass")).clear();
+    dr.findElement(By.name("pass")).sendKeys("secret");
+    dr.findElement(By.xpath("//input[@value='Login']")).click();
+    GoToContactPage();
+    VvodFirstname();
+    VvodMiddlename();
+    VvodLastname();
+    VvodNickname();
+    Vvodaddress();
+    Vvodhome();
+    VvodEmail();
+    SubmitContactCreation();
   }
 
-  @AfterClass(alwaysRun = true)
+  private void SubmitContactCreation() {
+    dr.findElement( org.openqa.selenium.By.xpath("//div[@id='content']/form/input[21]")).click();
+  }
+
+  private void VvodEmail() {
+    dr.findElement( org.openqa.selenium.By.name("email")).clear();
+    dr.findElement( org.openqa.selenium.By.name("email")).sendKeys("test7");
+  }
+
+  private void Vvodhome() {
+    dr.findElement( org.openqa.selenium.By.name("home")).clear();
+    dr.findElement( org.openqa.selenium.By.name("home")).sendKeys("test6");
+  }
+
+  private void Vvodaddress() {
+    dr.findElement( org.openqa.selenium.By.name("address")).clear();
+    dr.findElement( org.openqa.selenium.By.name("address")).sendKeys("test5");
+  }
+
+  private void VvodNickname() {
+    dr.findElement( org.openqa.selenium.By.name("nickname")).clear();
+    dr.findElement( org.openqa.selenium.By.name("nickname")).sendKeys("test4");
+  }
+
+  private void VvodLastname() {
+    dr.findElement( org.openqa.selenium.By.name("lastname")).clear();
+    dr.findElement( org.openqa.selenium.By.name("lastname")).sendKeys("test3");
+  }
+
+  private void VvodMiddlename() {
+    dr.findElement( org.openqa.selenium.By.name("middlename")).clear();
+    dr.findElement( org.openqa.selenium.By.name("middlename")).sendKeys("test2");
+  }
+
+  private void VvodFirstname() {
+    dr.findElement( org.openqa.selenium.By.name("firstname")).clear();
+    dr.findElement( org.openqa.selenium.By.name("firstname")).sendKeys("test1");
+  }
+
+  private void GoToContactPage() {
+    dr.findElement( org.openqa.selenium.By.linkText("add new")).click();
+  }
+
+  @AfterMethod(alwaysRun = true)
   public void tearDown() throws Exception {
-    driver.quit();
+    dr.quit();
     }
 
 
   private boolean isElementPresent(By by) {
     try {
-      driver.findElement(by);
+      dr.findElement(by);
       return true;
     } catch (NoSuchElementException e) {
       return false;
@@ -64,7 +95,7 @@ public class ContactCreationTests {
 
   private boolean isAlertPresent() {
     try {
-      driver.switchTo().alert();
+      dr.switchTo().alert();
       return true;
     } catch (NoAlertPresentException e) {
       return false;
