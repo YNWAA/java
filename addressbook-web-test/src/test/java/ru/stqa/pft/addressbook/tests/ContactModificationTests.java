ff4e18c808f0fa6f7eaf94d6a7bb320d8832a00a
YNWAA <pele_2092@mail.ru>
2021-08-27 16:08:23 +0300

    Задание 9

--- addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
+++ addressbook-web-test/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTests.java
@@ -13,13 +13,20 @@ public class ContactModificationTests extends TestBase {
         }
         java.util.List<ru.stqa.pft.addressbook.model.ContactData> before = app.getContactHelper().getContactList();
         app.getContactHelper().selectContactModification(before.size() - 1);
-        app.getContactHelper().fillform( new ContactData( "test1", "test2", "test3", "test4", "test5", "test6", "test7",null ),false );
+        ContactData contact=new ru.stqa.pft.addressbook.model.ContactData( "test1", "test2", "test3", "test4", "test5", "test6", "test7" ,"test1");
+
+        app.getContactHelper().fillform( contact,false );
         app.getContactHelper().updateContactModification();
         app.getContactHelper().goToContactPage();
         java.util.List<ru.stqa.pft.addressbook.model.ContactData> after = app.getContactHelper().getContactList();
-        org.testng.Assert.assertEquals(after.size(), before.size());
-        before.remove(before.size() - 1);
-        org.testng.Assert.assertEquals(new java.util.HashSet<Object>(before), new java.util.HashSet<Object>(after));
+        org.testng.Assert.assertEquals(after.size(),before.size());
+        before.remove(before.size()-1);
+        java.util.Comparator<? super ru.stqa.pft.addressbook.model.ContactData> byId = (java.util.Comparator<ru.stqa.pft.addressbook.model.ContactData>) (o1, o2) -> Integer.compare(o1.getId(), o2.getId());
+        before.add(contact);
+        before.sort(byId);
+        after.sort(byId);
+        org.testng.Assert.assertEquals(before,after);
+        org.testng.Assert.assertEquals(new java.util.HashSet<Object>(before),new java.util.HashSet<Object>(after));
     }
 
 }
